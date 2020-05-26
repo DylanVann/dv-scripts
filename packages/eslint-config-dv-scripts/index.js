fcf82e33c5113dd42cd3d5ab3bd38e47b0985ad2
Dylan Vann <dylan@dylanvann.com>
2020-05-25 20:54:14 -0400

    member-delimeter-style

--- packages/eslint-config-dv-scripts/index.js
+++ packages/eslint-config-dv-scripts/index.js
@@ -1,13 +1,24 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended", "react-app"],
-  plugins: ["@typescript-eslint", "react"],
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'react-app'],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/ban-ts-ignore": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
+    'no-unused-vars': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+        },
+        singleline: {
+          delimiter: 'comma',
+        },
+      },
+    ],
   },
-};
+}
