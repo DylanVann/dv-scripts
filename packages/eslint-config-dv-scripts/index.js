6244f829a7dc85603a9b740a52d7e35fa7757cd5
Dylan Vann <dylan@dylanvann.com>
2020-05-26 23:26:24 -0400

    fix member delimiter

--- packages/eslint-config-dv-scripts/index.js
+++ packages/eslint-config-dv-scripts/index.js
@@ -9,16 +9,6 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      {
-        multiline: {
-          delimiter: 'none',
-        },
-        singleline: {
-          delimiter: 'semi',
-        },
-      },
-    ],
+    '@typescript-eslint/member-delimiter-style': 'off',
   },
 }
