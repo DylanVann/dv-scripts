d8dd45acd217e054b3a7ede84c52ae3e85c9ebbb
Dylan Vann <dylan@dylanvann.com>
2020-06-18 04:21:19 -0400

    feat: add release command (#2)

---
+++ packages/dv-scripts/src/configs/semanticRelease.ts
@@ -0,0 +1,19 @@
+const config = {
+  release: {
+    branch: 'master',
+    verifyConditions: [
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+    ],
+    prepare: [
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+    ],
+    publish: ['@semantic-release/npm', '@semantic-release/github'],
+  },
+}
+
+module.exports = config
+export {}
---
+++ packages/dv-scripts/src/scripts/release.ts
@@ -0,0 +1,41 @@
+import execa from 'execa'
+import findYarnWorkspaceRoot from 'find-yarn-workspace-root'
+
+export const release = async ({
+  gitHubToken,
+  npmToken,
+  args,
+}: {
+  gitHubToken?: string
+  npmToken?: string
+  args: string[]
+}) => {
+  const isInWorkspace = findYarnWorkspaceRoot() !== null
+  if (!gitHubToken) {
+    throw new Error(
+      `We need the GITHUB_TOKEN environment variable to be set to perform a release.`,
+    )
+  }
+
+  if (!npmToken) {
+    throw new Error(
+      `We need the NPM_TOKEN environment variable to be set to perform a release.`,
+    )
+  }
+
+  const execaOptions = {
+    preferLocal: true,
+    stdio: 'inherit',
+  } as const
+
+  const extendsArgs = [
+    '--extends',
+    require.resolve('../configs/semanticRelease.js'),
+  ]
+
+  if (isInWorkspace) {
+    await execa('multi-semantic-release', [...extendsArgs], execaOptions)
+  } else {
+    await execa('semantic-release', [...extendsArgs], execaOptions)
+  }
+}
