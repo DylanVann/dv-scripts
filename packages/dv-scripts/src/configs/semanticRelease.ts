fa2a7972b8fbcae2be9a58b630e200cd1afb81e5
Dylan Vann <dylan@dylanvann.com>
2020-07-14 17:18:47 -0400

    fix: commit release notes and versions

--- packages/dv-scripts/src/configs/semanticRelease.ts
+++ packages/dv-scripts/src/configs/semanticRelease.ts
@@ -1,11 +1,54 @@
 const config = {
   branch: 'master',
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'build',
+            release: 'patch',
+          },
+          {
+            type: 'ci',
+            release: 'patch',
+          },
+          {
+            type: 'chore',
+            release: 'patch',
+          },
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
+          {
+            type: 'style',
+            release: 'patch',
+          },
+          {
+            type: 'test',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/git',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          // This is not a JS template string, it is processed by semantic-release.
+          // eslint-disable-next-line no-template-curly-in-string
+          'release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
     '@semantic-release/github',
   ],
 }
