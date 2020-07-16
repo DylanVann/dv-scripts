c03e9ac218c5f5c4b6599a3af4d8b71c9e9d75a8
Dylan Vann <dylan@dylanvann.com>
2020-07-16 18:16:25 -0400

    fix: make jest work better

--- packages/dv-scripts/src/scripts/test.ts
+++ packages/dv-scripts/src/scripts/test.ts
@@ -1,10 +1,11 @@
 import isCI from 'is-ci'
 import { readPkgUp } from '../utils'
+import execa from 'execa'
 import jestConfig = require('../configs/jest')
 
 export const test = async (args: string[]) => {
   const pkg = await readPkgUp()
-  const watch: string[] =
+  const watchArgs: string[] =
     !isCI &&
     !args.includes('--no-watch') &&
     !args.includes('--coverage') &&
@@ -12,20 +13,27 @@ export const test = async (args: string[]) => {
       ? ['--watch']
       : []
 
-  const config: string[] =
+  const configArgs: string[] =
     args.includes('--config') || pkg.packageJson.jest
       ? []
       : ['--config', JSON.stringify(jestConfig)]
 
   // CircleCI issues: https://discuss.circleci.com/t/jest-out-of-memory-error/32263
-  const maxWorkers = isCI ? ['--maxWorkers=2'] : []
+  const maxWorkersArgs = isCI ? ['--maxWorkers=2'] : []
   process.env.BABEL_ENV = 'test'
   process.env.NODE_ENV = 'test'
-  require('jest').run([
-    ...config,
-    ...watch,
-    '--passWithNoTests',
-    ...maxWorkers,
-    ...args,
-  ])
+  await execa(
+    'jest',
+    [
+      ...configArgs,
+      ...watchArgs,
+      '--passWithNoTests',
+      ...maxWorkersArgs,
+      ...args,
+    ],
+    {
+      preferLocal: true,
+      stdio: 'inherit',
+    },
+  )
 }
